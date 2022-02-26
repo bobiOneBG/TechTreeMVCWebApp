bcc980db7f8ff3f1119b6cf7ca0467079fbdef08
bobiOne <bobione@abv.bg>
2022-02-26 17:57:22 +0200

    UserToCategory base

---
+++ TechTreeMVCWebApplication/Areas/Admin/Models/UsersCategoryListModel.cs
@@ -0,0 +1,11 @@
+﻿namespace TechTreeMVCWebApplication.Areas.Admin.Models
+{
+    public class UsersCategoryListModel
+    {
+        public int CategoryId { get; set; }
+
+        public ICollection<UserModel> Users { get; set; }
+
+        public ICollection<UserModel> UsersSelected { get; set; }
+    }
+}
---
+++ TechTreeMVCWebApplication/Comparers/CompareUsers.cs
@@ -0,0 +1,24 @@
+﻿namespace TechTreeMVCWebApplication.Comparers
+{
+    using System.Diagnostics.CodeAnalysis;
+    using TechTreeMVCWebApplication.Areas.Admin.Models;
+
+    public class CompareUsers
+    {
+        public bool Equals(UserModel x, UserModel y)
+        {
+            if (y == null) return false;
+
+            if (x.Id == y.Id)
+                return true;
+
+            return false;
+
+        }
+
+        public int GetHashCode([DisallowNull] UserModel obj)
+        {
+            return obj.Id.GetHashCode();
+        }
+    }
+}
